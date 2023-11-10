101a10aa3bdb646cb56ffe5f424f527335719bd8
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2023-11-10 09:08:04 +0900

    context api로 사용자 정보 관리하기

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 export default function Profile() {
-	const user = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 	console.log(user);
 
 	const onSignOut = async () => {
@@ -25,8 +25,8 @@ export default function Profile() {
 			<div className="flex__box-lg">
 				<div className="profile__image" />
 				<div>
-					{/* <div className="profile__email">{auth?.currentUser?.email}</div> */}
-					{/* <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div> */}
+					<div className="profile__email">{user?.email}</div>
+					<div className="profile__name">{user?.displayName || '사용자'}</div>
 				</div>
 			</div>
 			<div role="presentation" className="profile__logout" onClick={onSignOut}>
